7fa1453fd2354d2eaa77096ad3caefed07b42425
yaskyj <yaskyj@yahoo.com>
2015-04-14 22:32:29 -0500

    Change name to dataDashboard and add traffic factory

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the zyringApp
  */
-angular.module('zyringApp')
+angular.module('dataDashboard')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('zyringApp'));
+  beforeEach(module('dataDashboard'));
 
   var MainCtrl,
     scope;
