5752f55b6a98b7739166e811f18c9ba1677da814
yaskyj <yaskyj@yahoo.com>
2015-04-15 19:13:03 -0500

    Setup promise between factory and controller

--- app/scripts/services/trafficFactory.js
+++ app/scripts/services/trafficFactory.js
@@ -1,8 +1,22 @@
 angular.module('dataDashboard')
   .factory('Traffic',[
-    '$resource',
-    //myObj will be created by calling this function
-    //and capturing its return value
-    function($resource){
-      return $resource('http://micky.zyring.com/fullEvents');
+    '$http',
+    '$q',
+    function($http, $q){
+      return {
+        getTraffic: function() {
+          return $http.get('http://micky.zyring.com/fullEvents')
+            .then(function(response) {
+              if (typeof response.data === 'object') {
+                return response.data;
+              }
+              else {
+                return $q.reject(response.data);
+              }
+            }, function(response) {
+                return $q.reject(response.data);
+            });
+        }
+      } 
+      // $resource('http://micky.zyring.com/fullEvents');
     }]);
