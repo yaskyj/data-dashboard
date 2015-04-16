67d9ecb4489bb1436da23cef25cabd54c1d30854
yaskyj <165812+yaskyj@users.noreply.github.com>
2015-04-15 22:31:44 -0500

    Add pie chart for visits per country

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,6 @@
 'use strict';
 dataDashboard.controller('MainCtrl', ['$scope', 'Traffic', function ($scope, Traffic) {
-    $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-    $scope.data = [300, 500, 100];
+
     var getAllTraffic = function() {
       Traffic.getTraffic()
         .then(function(data) {
@@ -9,7 +8,9 @@ dataDashboard.controller('MainCtrl', ['$scope', 'Traffic', function ($scope, Tra
             $scope.trafficList = data;
             // console.log($scope.trafficList);
             $scope.countryCount = _.countBy($scope.trafficList, 'country');
-            console.log($scope.countryCount);
+            // console.log($scope.countryCount);
+            $scope.countries = _.map($scope.countryCount, function(a, b) {return b ? b : "Unknown";});
+            $scope.visits = _.map($scope.countryCount, function(a, b) {return a;});
           }
         }, function(error) {
             console.log(error);
