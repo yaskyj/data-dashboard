5200a030fb51f4c169eba9f7f358ade407864818
yaskyj <165812+yaskyj@users.noreply.github.com>
2015-04-15 19:13:03 -0500

    Setup promise between factory and controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,17 @@
  */
 angular.module('dataDashboard')
   .controller('MainCtrl', ['$scope', 'Traffic', function ($scope, Traffic) {
-      $scope.trafficList = Traffic.query();
-      console.log($scope.trafficList);
-    }]);
+    var getAllTraffic = function() {
+      Traffic.getTraffic()
+        .then(function(data) {
+          console.log("This");
+          if (data) {
+            $scope.trafficList = data
+            console.log($scope.trafficList);
+          }
+        }, function(error) {
+            console.log(error);
+        });
+    }();
+    console.log("This");
+  }]);
