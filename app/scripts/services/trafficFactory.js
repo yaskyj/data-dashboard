d66b9d4a233d7846c1d7ecbc2079740ad9325810
yaskyj <165812+yaskyj@users.noreply.github.com>
2015-04-19 00:52:19 -0500

    Change  to

--- app/scripts/services/trafficFactory.js
+++ app/scripts/services/trafficFactory.js
@@ -1,22 +1,6 @@
 'use strict';
 dataDashboard.factory('Traffic',[
-    '$http',
-    '$q',
-    function($http, $q){
-      return {
-        getTraffic: function() {
-          return $http.get('http://micky.zyring.com/fullEvents')
-            .then(function(response) {
-              if (typeof response.data === 'object') {
-                return response.data;
-              }
-              else {
-                return $q.reject(response.data);
-              }
-            }, function(response) {
-                return $q.reject(response.data);
-            });
-        }
-      } 
-      // $resource('http://micky.zyring.com/fullEvents');
-    }]);
+  '$resource',
+  function($resource) {
+    return $resource('http://micky.zyring.com/fullEvents');
+  }]);
