de71fa2b9af446ac523f64e99912663ce571a891
yaskyj <165812+yaskyj@users.noreply.github.com>
2015-04-19 09:38:17 -0500

    Add initial divs for datepickers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,9 +5,7 @@ dataDashboard
       Traffic.query(function(data) {
         if (data) {
           $scope.trafficList = data;
-          // console.log($scope.trafficList);
           $scope.countryCount = _.countBy($scope.trafficList, 'country');
-          // console.log($scope.countryCount);
           $scope.countries = _.keys($scope.countryCount);
           $scope.visits = _.values($scope.countryCount);        
         }
