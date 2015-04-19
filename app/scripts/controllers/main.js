4fda5b7a6bb926cedee074bf66476e8488d6c4f9
yaskyj <yaskyj@yahoo.com>
2015-04-19 00:52:19 -0500

    Change  to

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,15 @@
 'use strict';
-dataDashboard.controller('MainCtrl', ['$scope', 'Traffic', function ($scope, Traffic) {
-
-    var getAllTraffic = function() {
-      Traffic.getTraffic()
-        .then(function(data) {
-          if (data) {
-            $scope.trafficList = data;
-            // console.log($scope.trafficList);
-            $scope.countryCount = _.countBy($scope.trafficList, 'country');
-            // console.log($scope.countryCount);
-            $scope.countries = _.keys($scope.countryCount);
-            $scope.visits = _.values($scope.countryCount);
-          }
-        }, function(error) {
-            console.log(error);
-        });
-    }();
-  }]);
+dataDashboard
+  .controller('MainCtrl', ['$scope', 'Traffic', 
+    function ($scope, Traffic) {
+      Traffic.query(function(data) {
+        if (data) {
+          $scope.trafficList = data;
+          // console.log($scope.trafficList);
+          $scope.countryCount = _.countBy($scope.trafficList, 'country');
+          // console.log($scope.countryCount);
+          $scope.countries = _.keys($scope.countryCount);
+          $scope.visits = _.values($scope.countryCount);        
+        }
+      });
+    }]);
