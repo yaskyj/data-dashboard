195dfe09da5bbf11a78159abc659bd3c76557091
yaskyj <yaskyj@yahoo.com>
2015-04-19 10:02:34 -0500

    Trying out timestamp conversion

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,8 @@ dataDashboard
           $scope.trafficList = data;
           $scope.countryCount = _.countBy($scope.trafficList, 'country');
           $scope.countries = _.keys($scope.countryCount);
-          $scope.visits = _.values($scope.countryCount);        
+          $scope.visits = _.values($scope.countryCount);
+          console.log(new Date($scope.trafficList[0].timestamp*1000));
         }
       });
     }]);
