d8e5e52203bcc06d071a45ab1e9478008a0d213c
yaskyj <yaskyj@yahoo.com>
2015-04-20 12:53:39 -0500

    Trying to get datepicker to work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 dataDashboard
-  .controller('MainCtrl', ['$scope', 'Traffic', 
+  .controller('MainCtrl', ['$scope', 'Traffic',
     function ($scope, Traffic) {
       Traffic.query(function(data) {
         if (data) {
@@ -12,37 +12,37 @@ dataDashboard
         }
       });
 
-        $scope.today = function() {
-        $scope.dt = new Date();
-        };
-        $scope.today();
+      $scope.today = function() {
+      $scope.dt = new Date();
+      };
+      $scope.today();
 
-        $scope.clear = function () {
-          $scope.dt = null;
-        };
+      $scope.clear = function () {
+        $scope.dt = null;
+      };
 
-        // Disable weekend selection
-        $scope.disabled = function(date, mode) {
-          return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-        };
+      // Disable weekend selection
+      $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+      };
 
-        $scope.toggleMin = function() {
-          $scope.minDate = $scope.minDate ? null : new Date();
-        };
-        $scope.toggleMin();
+      $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+      };
+      $scope.toggleMin();
 
-        $scope.open = function($event) {
-          $event.preventDefault();
-          $event.stopPropagation();
+      $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
 
-          $scope.opened = true;
-        };
+        $scope.opened = true;
+      };
 
-        $scope.dateOptions = {
-          formatYear: 'yy',
-          startingDay: 1
-        };
+      $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+      };
 
-        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.format = $scope.formats[0];
+      $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+      $scope.format = $scope.formats[0];
     }]);
