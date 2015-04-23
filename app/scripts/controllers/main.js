a2da3658c1928255304f8457c9220ee5a2ee06a6
yaskyj <yaskyj@yahoo.com>
2015-04-23 15:02:10 -0500

    Visits by week complete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,35 +7,35 @@ Date.prototype.getWeek = function() {
 dataDashboard
   .controller('MainCtrl', ['$scope', 'Traffic',
     function ($scope, Traffic) {
+      //Function which updates objects based on changes in dates
       var updateCurrentTraffic = function() {
+
+        //Gets all current traffic between specified dates
         $scope.currentTraffic = _.filter($scope.trafficList, function(item) {
           return ((item.timestamp * 1000) >= Date.parse($scope.dtStart)) && ((item.timestamp * 1000) <= Date.parse($scope.dtEnd));
         });
+
         //Visits by country
         $scope.countryCount = _.countBy($scope.currentTraffic, 'country');
         $scope.countries = _.keys($scope.countryCount);
         $scope.visits = _.values($scope.countryCount);
 
         //Visits by week
-        $scope.weeklyCount = _.sortBy(_.groupBy($scope.currentTraffic, function(item) {
+        $scope.weeklyCount = _.groupBy($scope.currentTraffic, function(item) {
           return new Date(item.timestamp * 1000).getWeek();
-        }), function(a, b) {
-          return parseInt(b);
         });
-        $scope.allWeeksInRange = _.range(1, $scope.dtEnd.getWeek() + 1);
-        console.log($scope.allWeeksInRange);
-        console.log($scope.dtStart.getWeek());
-        console.log($scope.dtEnd.getWeek());        
-        $scope.labels = _.map($scope.weeklyCount, function(a, b) {
+        $scope.allWeeksInRange = _.range($scope.dtStart.getWeek(), $scope.dtEnd.getWeek() + 1);
+        $scope.countByWeekRange = _.map($scope.allWeeksInRange, function(a) {
+          return a in $scope.weeklyCount ? $scope.weeklyCount[a].length : 0;
+        });
+        $scope.countByWeekRange = [$scope.countByWeekRange];
+        $scope.weekLabels = _.map($scope.allWeeksInRange, function(a, b) {
           return 'Week ' + (b + 1);
         });
-        $scope.series = ['Visits'];
-        $scope.data = _.map($scope.weeklyCount, function(a) {
-          return a.length;
-        });
-        $scope.data = [$scope.data];
+        $scope.weekSeries = ['Visits'];
       };
 
+      //Date settings
       $scope.today = function() {
         var currentYear = new Date().getFullYear();
         $scope.dtStart = new Date(currentYear,0,1);
@@ -66,11 +66,12 @@ dataDashboard
         startingDay: 1
       };
 
+      //Watches for changes in dates
       $scope.$watch('[dtStart, dtEnd]', function() {
         updateCurrentTraffic();
       }, true);
 
-      // $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+      //Query to get all traffic from API
       Traffic.query(function(data) {
         if (data) {
           $scope.trafficList = data;
