97bab2a4548678b87ae5f0db7938114beba1ee7f
yaskyj <165812+yaskyj@users.noreply.github.com>
2015-04-29 21:04:22 -0500

    Completed some changes based on the code review comments

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,7 @@
 Date.prototype.getWeek = function() {
         var onejan = new Date(this.getFullYear(), 0, 1);
         return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
-}
+};
 
 dataDashboard
   .controller('MainCtrl', ['$scope', 'Traffic',
@@ -76,6 +76,9 @@ dataDashboard
         if (data) {
           $scope.trafficList = data;
           updateCurrentTraffic();
+        }
+        else {
+          console.log('There was an error!');
         }
       });
     }]);
