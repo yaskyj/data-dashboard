c74fe63243821746b6cc59a5738ad0a8bb92741a
yaskyj <165812+yaskyj@users.noreply.github.com>
2015-05-07 18:04:32 -0500

    User Story 5 - added map by country visits and started refactoring code into separate controllers

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,11 +7,7 @@
  * # AboutCtrl
  * Controller of the zyringApp
  */
-angular.module('dataDashboard')
+dataDashboard
   .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    console.log("About test!");
   });
